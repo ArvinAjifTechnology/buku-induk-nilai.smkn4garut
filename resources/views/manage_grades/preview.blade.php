1ead77bb7394ee434d3c83aff643ea36ac68848c
Arvin Muhammad Ajif <arvinajif3@gmail.com>
2024-10-26 20:15:56 +0700

    "Refactored preview.blade.php to display import file details and student data in a table"

--- resources/views/manage_grades/preview.blade.php
+++ resources/views/manage_grades/preview.blade.php
@@ -2,53 +2,45 @@
 
 @section('content')
     <div class="container">
-        <h3>Preview Data Import</h3>
+        <h3>Preview Data Import - File: {{ $import['file_name'] }}</h3>
+        <p><strong>Kelas:</strong> {{ $import['class'] }}</p>
+        <p><strong>Tahun Ajaran:</strong> {{ $import['yearRange'] }}</p>
+        <p><strong>Semester:</strong> {{ $import['semester'] }}</p>
+        <p><strong>Total Siswa:</strong> {{ count($import['data']) }}</p>
 
-        @foreach (session('import_data') as $import)
-            <div class="mb-5">
-                <h4>File: {{ $import['file_name'] }}</h4>
-                <p><strong>Kelas:</strong> {{ $import['class'] }}</p>
-                <p><strong>Tahun Ajaran:</strong> {{ $import['yearRange'] }}</p>
-                <p><strong>Semester:</strong> {{ $import['semester'] }}</p>
-                <p><strong>Total Siswa yang Akan Diimpor:</strong> {{ count($import['data']) }}</p>
-                <p><strong>Total Mata Pelajaran:</strong> {{ count($import['data'][0]['scores']) }}</p>
-
-                <h5>Data Siswa</h5>
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th>Nama Siswa</th>
-                            <th>NISN</th>
-                            <th>Jurusan</th>
-                            <th>Kelas</th>
-                            <th>Nilai Mata Pelajaran</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($import['data'] as $data)
-                            <tr>
-                                <td>{{ $data['name'] }}</td>
-                                <td>{{ $data['nisn'] }}</td>
-                                <td>{{ $data['major'] }}</td>
-                                <td>{{ $data['entryYear'] . '/' . $data['schoolClass'] }}</td>
-                                <td>
-                                    <ul>
-                                        @foreach ($data['scores'] as $scoreData)
-                                            <li>{{ $scoreData['subject'] }}: {{ $scoreData['score'] }}</li>
-                                        @endforeach
-                                    </ul>
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
-        @endforeach
+        <h5>Data Siswa</h5>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Nama Siswa</th>
+                    <th>NISN</th>
+                    <th>Kelas</th>
+                    <th>Jurusan</th>
+                    <th>Nilai</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($import['data'] as $data)
+                    <tr>
+                        <td>{{ $data['name'] }}</td>
+                        <td>{{ $data['nisn'] }}</td>
+                        <td>{{ $data['schoolClass'] }}</td>
+                        <td>{{ $data['major'] }}</td>
+                        <td>
+                            <ul>
+                                @foreach ($data['scores'] as $score)
+                                    <li>{{ $score['subject'] }}: {{ $score['score'] }}</li>
+                                @endforeach
+                            </ul>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
 
         <form action="{{ route('students-grades-e-raport-confirm-import') }}" method="POST">
             @csrf
-            <button type="submit" class="btn btn-success">Konfirmasi dan Simpan</button>
-            <a href="{{ url()->previous() }}" class="btn btn-secondary">Kembali</a>
+            <button type="submit" class="btn btn-success">Konfirmasi dan Lanjutkan</button>
         </form>
     </div>
 @endsection
