7f6866f69f6b18bfa2549ed037ee4ec7ab213a03
Arvin Muhammad Ajif <arvinajif3@gmail.com>
2024-10-26 20:03:27 +0700

    "Update variable name in previewImport method from 'file' to 'files'"

--- app/Http/Controllers/ManageGradeController.php
+++ app/Http/Controllers/ManageGradeController.php
@@ -256,7 +256,7 @@ class ManageGradeController extends Controller
 
     public function previewImport(Request $request)
     {
-        $files = $request->file('file'); // Ambil semua file
+        $files = $request->file('files'); // Ambil semua file
         $allImportData = [];
 
         foreach ($files as $file) {
