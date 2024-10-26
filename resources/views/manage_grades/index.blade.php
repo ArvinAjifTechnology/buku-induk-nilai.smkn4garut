06f8189b6db0acd5890877d9f995a1a6eda513db
Arvin Muhammad Ajif <arvinajif3@gmail.com>
2024-10-26 22:48:53 +0700

    "Refactored import modal and button layout in manage grades index view"

--- resources/views/manage_grades/index.blade.php
+++ resources/views/manage_grades/index.blade.php
@@ -2,40 +2,43 @@
 
 @section('content')
     <div class="container">
-        <div class="row">
+        <div class="row mb-4">
             <!-- Tombol Import -->
-            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#importModal">
-                Import Nilai dari E-Raport
-            </button>
+            <div class="col-md-12">
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#importModal">
+                    <i class="fas fa-upload"></i> Import Nilai dari E-Raport
+                </button>
+            </div>
+        </div>
 
-            <!-- Modal Import -->
-            <div class="modal fade" id="importModal" tabindex="-1" aria-labelledby="importModalLabel" aria-hidden="true">
-                <div class="modal-dialog">
-                    <form action="{{ route('students-grades-e-raport-preview-import') }}" method="POST"
-                        enctype="multipart/form-data">
-                        @csrf
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="importModalLabel">Import Nilai</h5>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                    aria-label="Close"></button>
-                            </div>
-                            <div class="modal-body">
-                                <div class="mb-3">
-                                    <label for="files" class="form-label">Upload File Excel (Banyak
-                                        File)</label>
-                                    <input type="file" name="files[]" class="form-control" required multiple
-                                        accept=".xlsx, .xls">
-                                </div>
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                <button type="submit" class="btn btn-primary">Preview</button>
+        <!-- Modal Import -->
+        <div class="modal fade" id="importModal" tabindex="-1" aria-labelledby="importModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <form action="{{ route('students-grades-e-raport-preview-import') }}" method="POST"
+                    enctype="multipart/form-data">
+                    @csrf
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="importModalLabel">Import Nilai</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <div class="mb-3">
+                                <label for="files" class="form-label">Upload File Excel (Banyak File)</label>
+                                <input type="file" name="files[]" class="form-control" required multiple
+                                    accept=".xlsx, .xls">
                             </div>
                         </div>
-                    </form>
-                </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
+                            <button type="submit" class="btn btn-primary">Preview</button>
+                        </div>
+                    </div>
+                </form>
             </div>
+        </div>
+
+        <div class="row">
             @foreach ($entryYears as $entryYear)
                 <div class="col-md-4 mb-4">
                     <div class="card shadow-sm">
@@ -45,8 +48,7 @@
                                 <i class="fas fa-calendar-alt"></i> {{ $entryYear->year }}
                             </h5>
                             <!-- Button with Icon -->
-                            <a href="{{ route('manage-grades.school-classes', $entryYear->uniqid) }}"
-                                class="btn btn-primary">
+                            <a href="{{ route('manage-grades.school-classes', $entryYear->uniqid) }}" class="btn btn-primary">
                                 <i class="fas fa-eye"></i> Lihat Daftar Kelas
                             </a>
                         </div>
