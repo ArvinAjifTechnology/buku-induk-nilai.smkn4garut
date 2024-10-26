a4c2537cea46240d3c5c29520bcfaae0d496d974
Arvin Muhammad Ajif <arvinajif3@gmail.com>
2024-10-26 22:30:22 +0700

    "Refactored table structure in preview.blade.php to dynamically generate subject headers and populate scores"

--- resources/views/manage_grades/preview.blade.php
+++ resources/views/manage_grades/preview.blade.php
@@ -10,35 +10,38 @@
 
         <h5>Data Siswa</h5>
         <table class="table">
-            <thead>
-                <tr>
-                    <th>No</th>
-                    <th>Nama Siswa</th>
-                    <th>NISN</th>
-                    <th>Kelas</th>
-                    <th>Jurusan</th>
-                    <th>Nilai</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($import['data'] as $data)
-                    <tr>
-                        <td>{{ $loop->iteration }}</td>
-                        <td>{{ $data['name'] }}</td>
-                        <td>{{ $data['nisn'] }}</td>
-                        <td>{{ $data['schoolClass'] }}</td>
-                        <td>{{ $data['major'] }}</td>
-                        <td>
-                            <ul>
-                                @foreach ($data['scores'] as $score)
-                                    <li>{{ $score['subject'] }}: {{ $score['score'] }}</li>
-                                @endforeach
-                            </ul>
-                        </td>
-                    </tr>
+    <thead>
+        <tr>
+            <th>No</th>
+            <th>Nama Siswa</th>
+            <th>NISN</th>
+            <th>Kelas</th>
+            <th>Jurusan</th>
+            <!-- Add dynamic subject headers -->
+            @if (count($import['data']) > 0)
+                @foreach ($import['data'][0]['scores'] as $score)
+                    <th>{{ $score['subject'] }}</th>
                 @endforeach
-            </tbody>
-        </table>
+            @endif
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($import['data'] as $data)
+            <tr>
+                <td>{{ $loop->iteration }}</td>
+                <td>{{ $data['name'] }}</td>
+                <td>{{ $data['nisn'] }}</td>
+                <td>{{ $data['schoolClass'] }}</td>
+                <td>{{ $data['major'] }}</td>
+                <!-- Loop through scores to populate the values -->
+                @foreach ($data['scores'] as $score)
+                    <td>{{ $score['score'] }}</td>
+                @endforeach
+            </tr>
+        @endforeach
+    </tbody>
+</table>
+
 
         <form action="{{ route('students-grades-e-raport-confirm-import') }}" method="POST" style="display: inline;">
             @csrf
