ade22ab0f1332153cec7eaef8486f8e8bd8225ef
Arvin Muhammad Ajif <arvinajif3@gmail.com>
2024-10-26 22:33:26 +0700

    "Wrapped table in div with class 'table-responsive' and adjusted indentation"

--- resources/views/manage_grades/preview.blade.php
+++ resources/views/manage_grades/preview.blade.php
@@ -9,40 +9,39 @@
         <p><strong>Total Siswa:</strong> {{ count($import['data']) }}</p>
 
         <h5>Data Siswa</h5>
-        <div style="max-height: 400px; overflow-y: auto;">
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th>No</th>
-                        <th>Nama Siswa</th>
-                        <th>NISN</th>
-                        <th>Kelas</th>
-                        <th>Jurusan</th>
-                        <!-- Add dynamic subject headers -->
-                        @if (count($import['data']) > 0)
-                            @foreach ($import['data'][0]['scores'] as $score)
-                                <th>{{ $score['subject'] }}</th>
-                            @endforeach
-                        @endif
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($import['data'] as $data)
-                        <tr>
-                            <td>{{ $loop->iteration }}</td>
-                            <td>{{ $data['name'] }}</td>
-                            <td>{{ $data['nisn'] }}</td>
-                            <td>{{ $data['schoolClass'] }}</td>
-                            <td>{{ $data['major'] }}</td>
-                            <!-- Loop through scores to populate the values -->
-                            @foreach ($data['scores'] as $score)
-                                <td>{{ $score['score'] }}</td>
-                            @endforeach
-                        </tr>
+        <div class="table-responsive" style="max-height: 400px; overflow-y: auto;">
+    <table class="table">
+        <thead>
+            <tr>
+                <th>No</th>
+                <th>Nama Siswa</th>
+                <th>NISN</th>
+                <th>Kelas</th>
+                <th>Jurusan</th>
+                @if (count($import['data']) > 0)
+                    @foreach ($import['data'][0]['scores'] as $score)
+                        <th>{{ $score['subject'] }}</th>
                     @endforeach
-                </tbody>
-            </table>
-        </div>
+                @endif
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($import['data'] as $data)
+                <tr>
+                    <td>{{ $loop->iteration }}</td>
+                    <td>{{ $data['name'] }}</td>
+                    <td>{{ $data['nisn'] }}</td>
+                    <td>{{ $data['schoolClass'] }}</td>
+                    <td>{{ $data['major'] }}</td>
+                    @foreach ($data['scores'] as $score)
+                        <td>{{ $score['score'] }}</td>
+                    @endforeach
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
+
 
 
 
