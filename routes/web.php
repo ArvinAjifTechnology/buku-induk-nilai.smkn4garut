fe6697187be69a95efaedd3672a3a25b976af972
Arvin Muhammad Ajif <arvinajif3@gmail.com>
2024-10-26 21:30:18 +0700

    "Removed empty line in routes/web.php"

--- routes/web.php
+++ routes/web.php
@@ -101,7 +101,6 @@ Route::middleware(['role:admin,student_affairs_staff'])->group(function () {
     Route::post('/merge', [StudentExportController::class, 'merge'])->name('merge');
 
 
-
     //import
     Route::post('/students/{student}/import-grades', [StudentController::class, 'importGrades'])->name('students.import-grades');
     Route::post('/students-grades-import', [ManageGradeController::class, 'import'])->name('students-grades-import');
