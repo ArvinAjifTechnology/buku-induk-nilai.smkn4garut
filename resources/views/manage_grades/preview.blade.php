c9dc59d58156e38d24238160f648f01df102da84
Arvin Muhammad Ajif <arvinajif3@gmail.com>
2024-10-26 22:33:36 +0700

    "Refactored table structure in preview.blade.php"

--- resources/views/manage_grades/preview.blade.php
+++ resources/views/manage_grades/preview.blade.php
@@ -10,37 +10,37 @@
 
         <h5>Data Siswa</h5>
         <div class="table-responsive" style="max-height: 400px; overflow-y: auto;">
-    <table class="table">
-        <thead>
-            <tr>
-                <th>No</th>
-                <th>Nama Siswa</th>
-                <th>NISN</th>
-                <th>Kelas</th>
-                <th>Jurusan</th>
-                @if (count($import['data']) > 0)
-                    @foreach ($import['data'][0]['scores'] as $score)
-                        <th>{{ $score['subject'] }}</th>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Nama Siswa</th>
+                        <th>NISN</th>
+                        <th>Kelas</th>
+                        <th>Jurusan</th>
+                        @if (count($import['data']) > 0)
+                            @foreach ($import['data'][0]['scores'] as $score)
+                                <th>{{ $score['subject'] }}</th>
+                            @endforeach
+                        @endif
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($import['data'] as $data)
+                        <tr>
+                            <td>{{ $loop->iteration }}</td>
+                            <td>{{ $data['name'] }}</td>
+                            <td>{{ $data['nisn'] }}</td>
+                            <td>{{ $data['schoolClass'] }}</td>
+                            <td>{{ $data['major'] }}</td>
+                            @foreach ($data['scores'] as $score)
+                                <td>{{ $score['score'] }}</td>
+                            @endforeach
+                        </tr>
                     @endforeach
-                @endif
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($import['data'] as $data)
-                <tr>
-                    <td>{{ $loop->iteration }}</td>
-                    <td>{{ $data['name'] }}</td>
-                    <td>{{ $data['nisn'] }}</td>
-                    <td>{{ $data['schoolClass'] }}</td>
-                    <td>{{ $data['major'] }}</td>
-                    @foreach ($data['scores'] as $score)
-                        <td>{{ $score['score'] }}</td>
-                    @endforeach
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-</div>
+                </tbody>
+            </table>
+        </div>
 
 
 
