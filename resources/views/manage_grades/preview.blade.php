d188439170c5f49c4f3392de6e580fd8d727dd90
Arvin Muhammad Ajif <arvinajif3@gmail.com>
2024-10-26 22:32:45 +0700

    "Refactored preview.blade.php to add div with max-height and overflow-y auto, moved table headers outside foreach loop, and reorganized table structure."

--- resources/views/manage_grades/preview.blade.php
+++ resources/views/manage_grades/preview.blade.php
@@ -9,38 +9,41 @@
         <p><strong>Total Siswa:</strong> {{ count($import['data']) }}</p>
 
         <h5>Data Siswa</h5>
-        <table class="table">
-            <thead>
+       <div style="max-height: 400px; overflow-y: auto;">
+    <table class="table">
+        <thead>
+            <tr>
+                <th>No</th>
+                <th>Nama Siswa</th>
+                <th>NISN</th>
+                <th>Kelas</th>
+                <th>Jurusan</th>
+                <!-- Add dynamic subject headers -->
+                @if (count($import['data']) > 0)
+                    @foreach ($import['data'][0]['scores'] as $score)
+                        <th>{{ $score['subject'] }}</th>
+                    @endforeach
+                @endif
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($import['data'] as $data)
                 <tr>
-                    <th>No</th>
-                    <th>Nama Siswa</th>
-                    <th>NISN</th>
-                    <th>Kelas</th>
-                    <th>Jurusan</th>
-                    <!-- Add dynamic subject headers -->
-                    @if (count($import['data']) > 0)
-                        @foreach ($import['data'][0]['scores'] as $score)
-                            <th>{{ $score['subject'] }}</th>
-                        @endforeach
-                    @endif
+                    <td>{{ $loop->iteration }}</td>
+                    <td>{{ $data['name'] }}</td>
+                    <td>{{ $data['nisn'] }}</td>
+                    <td>{{ $data['schoolClass'] }}</td>
+                    <td>{{ $data['major'] }}</td>
+                    <!-- Loop through scores to populate the values -->
+                    @foreach ($data['scores'] as $score)
+                        <td>{{ $score['score'] }}</td>
+                    @endforeach
                 </tr>
-            </thead>
-            <tbody>
-                @foreach ($import['data'] as $data)
-                    <tr>
-                        <td>{{ $loop->iteration }}</td>
-                        <td>{{ $data['name'] }}</td>
-                        <td>{{ $data['nisn'] }}</td>
-                        <td>{{ $data['schoolClass'] }}</td>
-                        <td>{{ $data['major'] }}</td>
-                        <!-- Loop through scores to populate the values -->
-                        @foreach ($data['scores'] as $score)
-                            <td>{{ $score['score'] }}</td>
-                        @endforeach
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
+            @endforeach
+        </tbody>
+    </table>
+</div>
+
 
 
         <form action="{{ route('students-grades-e-raport-confirm-import') }}" method="POST" style="display: inline;">
