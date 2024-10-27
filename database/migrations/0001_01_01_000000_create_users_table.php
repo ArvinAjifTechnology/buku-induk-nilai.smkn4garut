2cf9f38fb785f7d468bf3d35e68a98d6e80ed36f
Arvin Muhammad Ajif <arvinajif3@gmail.com>
2024-10-28 06:10:15 +0700

    Added softDeletes to users table in create_users_table migration.

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration {
             $table->enum('role',['admin', 'student_affairs_staff']);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->softDeletes();
             $table->rememberToken();
             $table->timestamps();
         });
