2932be2fd9c98e12590e509c7a060e05649c99f5
Arvin Muhammad Ajif <arvinajif3@gmail.com>
2024-10-28 06:13:02 +0700

    Added softDeletes to graduation_years table.

--- database/migrations/2024_08_25_160631_create_graduation_years_table.php
+++ database/migrations/2024_08_25_160631_create_graduation_years_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('uniqid')->unique();
             $table->string('year')->unique();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
