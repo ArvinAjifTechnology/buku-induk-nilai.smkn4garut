62b9891bb2b3c4e48f18f67721cdedbe9ad69303
Arvin Muhammad Ajif <arvinajif3@gmail.com>
2024-10-28 06:14:57 +0700

    "Added SoftDeletes trait to GraduationYear model"

--- app/Models/GraduationYear.php
+++ app/Models/GraduationYear.php
@@ -5,11 +5,13 @@ namespace App\Models;
 use App\Models\Student;
 use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class GraduationYear extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     protected $guarded = ['id'];
 
