c683b88f28b1013f149f3d5e37a942ca111efce1
Arvin Muhammad Ajif <arvinajif3@gmail.com>
2024-10-28 06:12:19 +0700

    Added softDeletes to entry_year_major_subject table.

--- database/migrations/2024_08_21_054415_create_entry_year_major_subject_table.php
+++ database/migrations/2024_08_21_054415_create_entry_year_major_subject_table.php
@@ -15,6 +15,7 @@ return new class extends Migration {
             $table->foreignId('subject_id');
             $table->foreignId('entry_year_id');
             $table->foreignId('major_id');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
