274e126dc92cd74aabcf96905c264d52ac69b46f
Arvin Muhammad Ajif <arvinajif3@gmail.com>
2024-11-02 04:53:19 +0700

    "Uncommented 'use SoftDeletes;' in Major, Student, Subject, and User models."

--- app/Models/Subject.php
+++ app/Models/Subject.php
@@ -10,7 +10,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Subject extends Model
 {
     use HasFactory;
-    // use SoftDeletes;
+    use SoftDeletes;
 
     protected $guarded = ['id'];
 
