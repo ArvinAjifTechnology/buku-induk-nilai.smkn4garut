b6fb49e4871f96d6abf870726b8778a85a8e19c5
Arvin Muhammad Ajif <arvinajif3@gmail.com>
2024-11-06 13:22:26 +0700

    "Comment out all event handling methods in StudentObserver class"

--- app/Observers/StudentObserver.php
+++ app/Observers/StudentObserver.php
@@ -6,45 +6,45 @@ use App\Models\Student;
 
 class StudentObserver
 {
-    /**
-     * Handle the Student "created" event.
-     */
-    public function created(Student $student): void
-    {
-        //
-    }
+    // /**
+    //  * Handle the Student "created" event.
+    //  */
+    // public function created(Student $student): void
+    // {
+    //     //
+    // }
 
-    /**
-     * Handle the Student "updated" event.
-     */
-    public function updated(Student $student): void
-    {
-        //
-    }
+    // /**
+    //  * Handle the Student "updated" event.
+    //  */
+    // public function updated(Student $student): void
+    // {
+    //     //
+    // }
 
-    /**
-     * Handle the Student "deleted" event.
-     */
-    public function deleted(Student $student): void
-    {
-        //
-    }
+    // /**
+    //  * Handle the Student "deleted" event.
+    //  */
+    // public function deleted(Student $student): void
+    // {
+    //     //
+    // }
 
-    /**
-     * Handle the Student "restored" event.
-     */
-    public function restored(Student $student): void
-    {
-        //
-    }
+    // /**
+    //  * Handle the Student "restored" event.
+    //  */
+    // public function restored(Student $student): void
+    // {
+    //     //
+    // }
 
-    /**
-     * Handle the Student "force deleted" event.
-     */
-    public function forceDeleted(Student $student): void
-    {
-        //
-    }
+    // /**
+    //  * Handle the Student "force deleted" event.
+    //  */
+    // public function forceDeleted(Student $student): void
+    // {
+    //     //
+    // }
 
     /**
      * Handle the Student "deleting" event.
