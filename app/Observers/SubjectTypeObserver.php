6621957d77f6047268cef481f0cddddaae6bfefc
Arvin Muhammad Ajif <arvinajif3@gmail.com>
2024-11-06 13:23:48 +0700

    "Removed empty event handler methods for Student model, commented out existing methods"

--- app/Observers/SubjectTypeObserver.php
+++ app/Observers/SubjectTypeObserver.php
@@ -7,45 +7,45 @@ use App\Models\SubjectType;
 
 class SubjectTypeObserver
 {
-    /**
-     * Handle the Student "created" event.
-     */
-    public function created(Student $student): void
-    {
-        //
-    }
+    // /**
+    //  * Handle the Student "created" event.
+    //  */
+    // public function created(Student $student): void
+    // {
+    //     //
+    // }
 
-    /**
-     * Handle the Student "updated" event.
-     */
-    public function updated(Student $student): void
-    {
-        //
-    }
+    // /**
+    //  * Handle the Student "updated" event.
+    //  */
+    // public function updated(Student $student): void
+    // {
+    //     //
+    // }
 
-    /**
-     * Handle the Student "deleted" event.
-     */
-    public function deleted(Student $student): void
-    {
-        //
-    }
+    // /**
+    //  * Handle the Student "deleted" event.
+    //  */
+    // public function deleted(Student $student): void
+    // {
+    //     //
+    // }
 
-    /**
-     * Handle the Student "restored" event.
-     */
-    public function restored(Student $student): void
-    {
-        //
-    }
+    // /**
+    //  * Handle the Student "restored" event.
+    //  */
+    // public function restored(Student $student): void
+    // {
+    //     //
+    // }
 
-    /**
-     * Handle the Student "force deleted" event.
-     */
-    public function forceDeleted(Student $student): void
-    {
-        //
-    }
+    // /**
+    //  * Handle the Student "force deleted" event.
+    //  */
+    // public function forceDeleted(Student $student): void
+    // {
+    //     //
+    // }
 
     /**
      * Handle the Subject "deleting" event.
